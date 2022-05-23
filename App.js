61f47b7a5f7b7b2740af7d759d5d8541de0534af
Uchchash346 <mru416@gmail.com>
2022-05-23 22:20:27 +0600

    Add theme and necessary packages

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+// import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
+import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
+import Text from './src/components/text/text';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  })
+
+  if (!loaded) {
+    return (
+      <Text>Font is loading...</Text>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text preset="h1" style={{ color: 'blue' }}>
+        Open up App.js to start working on your app!</Text>
+      <Text preset="small" style={{ marginTop: spacing[4] }}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'black',
   },
 });
